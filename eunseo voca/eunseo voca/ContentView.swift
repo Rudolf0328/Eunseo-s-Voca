284e714893acaf1cfa1f0181db31615d37a447a3
rudolf0328 <ck0716080@gmail.com>
2022-05-26 15:28:03 +0900

    feat: hamburger bar

--- eunseo voca/eunseo voca/ContentView.swift
+++ eunseo voca/eunseo voca/ContentView.swift
@@ -1,14 +1,100 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var showMenu = false
+    
     var body: some View {
-        VStack(spacing: 3) {
-            Color.white.frame(height: 80)
-            HStack(spacing: 3) {
-                Color.white.frame(width: 150)
-                Color.white
-            }.background(.black)
-        }.background(.black)
+        
+        let drag = DragGesture()
+                    .onEnded {
+                        if $0.translation.width < -100 {
+                            withAnimation {
+                                self.showMenu = false
+                            }
+                        }
+                    }
+        
+        return NavigationView {
+            GeometryReader { geometry in
+                ZStack(alignment: .leading) {
+                    MainView(showMenu: self.$showMenu)
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+                        .offset(x: self.showMenu ? geometry.size.width/2 : 0)
+                        .disabled(self.showMenu ? true : false)
+                    if self.showMenu {
+                        MenuView()
+                            .frame(width: geometry.size.width/2)
+                            .transition(.move(edge: .leading))
+                    }
+                }
+                .gesture(drag)
+            }
+            .navigationBarTitle("Side Menu", displayMode: .inline)
+            .navigationBarItems(leading: (
+                Button(action: {
+                    withAnimation {
+                        self.showMenu.toggle()
+                    }
+                }) {
+                    Image(systemName: "line.horizontal.3")
+                        .imageScale(.large)
+                }
+            ))
+        }
+    }
+}
+
+struct MainView: View {
+    @Binding var showMenu: Bool
+    
+    var body: some View {
+        Button(action: {
+            withAnimation {
+                self.showMenu = true
+            }
+        }) {
+            Text("Show Menu")
+        }
+    }
+}
+
+struct MenuView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Image(systemName: "person")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Profile")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 100)
+            HStack {
+                Image(systemName: "envelope")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Messages")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 30)
+            HStack {
+                Image(systemName: "gear")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Settings")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 30)
+            Spacer()
+        }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .background(Color(red: 32/255, green: 32/255, blue: 32/255))
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
