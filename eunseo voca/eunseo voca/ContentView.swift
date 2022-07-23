5eca08c3f3029be5fb5bd4a81cd1c44284a1cd42
Rudolf0328 <54528011+Rudolf0328@users.noreply.github.com>
2022-07-23 19:50:57 +0900

    1 navigation bar (#2)
    * feat: hamburger bar
    
    * feat: function declaration and call
    
    * changes

--- eunseo voca/eunseo voca/ContentView.swift
+++ eunseo voca/eunseo voca/ContentView.swift
@@ -1,14 +1,124 @@
 import SwiftUI
 
+var wordPair: [String: String] = [:]
+
+func initializeVoca() {
+    wordPair["flower"] = "꽃"
+    wordPair["car"] = "차"
+    wordPair["dog"] = "강아지"
+    wordPair["cat"] = "고양이"
+    wordPair["animal"] = "동물"
+    wordPair["clock"] = "시계"
+    wordPair["water"] = "물"
+    wordPair["frog"] = "개구리"
+    wordPair["fire"] = "불"
+    wordPair["food"] = "음식"
+}
+
 struct ContentView: View {
+    
+    @State var showMenu = false
+    
     var body: some View {
-        VStack(spacing: 3) {
-            Color.white.frame(height: 80)
-            HStack(spacing: 3) {
-                Color.white.frame(width: 150)
-                Color.white
-            }.background(.black)
-        }.background(.black)
+        
+        // 화면 로딩 될 때마다 initialization 되는 중
+        initializeVoca()
+        
+        let drag = DragGesture()
+                    .onEnded {
+                        if $0.translation.width < -100 {
+                            withAnimation {
+                                self.showMenu = false
+                            }
+                        }
+                    }
+        
+        return NavigationView {
+            GeometryReader { geometry in
+                ZStack(alignment: .leading) {
+                    MainView(showMenu: self.$showMenu)
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+                        .offset(x: self.showMenu ? geometry.size.width/2 : 0)
+                        .disabled(self.showMenu ? true : false)
+                    if self.showMenu {
+                        MenuView()
+                            .frame(width: geometry.size.width/2)
+                            .transition(.move(edge: .leading))
+                    }
+                }
+                .gesture(drag)
+            }
+            .navigationBarTitle("Eunseo's Voca", displayMode: .inline)
+            .navigationBarItems(leading: (
+                Button(action: {
+                    withAnimation {
+                        self.showMenu.toggle()
+                    }
+                }) {
+                    Image(systemName: "line.horizontal.3")
+                        .imageScale(.large)
+                }
+            ))
+        }
+        .navigationViewStyle(.stack)
+    }
+}
+
+struct MainView: View {
+    @Binding var showMenu: Bool
+    
+    var body: some View {
+//        
+//        for (key, value) in wordPair {
+//            
+//        }
+//        
+        Button(action: {
+            withAnimation {
+                self.showMenu = true
+            }
+        }) {
+            Text("Show Menu")
+        }
+    }
+}
+
+struct MenuView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Image(systemName: "person")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Profile")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 100)
+            HStack {
+                Image(systemName: "envelope")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Messages")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 30)
+            HStack {
+                Image(systemName: "gear")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Settings")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 30)
+            Spacer()
+        }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .background(Color(red: 32/255, green: 32/255, blue: 32/255))
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
